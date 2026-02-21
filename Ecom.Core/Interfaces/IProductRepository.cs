b66df127b512c29ddd21204b884258a8130717fb
mohamed.allam <mohamed.allam@MOHAMED-ALLAM>
2026-02-21 13:45:42 +0200

    Add Repos and UnitOfWork

---
+++ Ecom.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+
+namespace Ecom.Core.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        // for future method
+    }
+}
---
+++ Ecom.Infrastructure/infrastructureRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecom.Infrastructure
+{
+    public static class infrastructureRegistration
+    {
+        public static IServiceCollection infrastructureConfigration(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
